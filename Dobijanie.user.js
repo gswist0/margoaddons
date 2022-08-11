2fc37988454fb2b24009e1b9f32431985cf11e2d
gswist0 <72512234+gswist0@users.noreply.github.com>
2022-08-12 00:21:05 +0200

    changed window position

--- Dobijanie.user.js
+++ Dobijanie.user.js
@@ -13,7 +13,7 @@ function run() {
 
     var gonitwa = document.createElement("div")
     gonitwa.id = "gonitwa"
-    gonitwa.style.cssText = "position:absolute;top:500px;left:50px;width:300px;height:500px;background-color:white;z-index:999;display:none;overflow-y:scroll;text-align:center";
+    gonitwa.style.cssText = "position:absolute;bottom:150px;right:50px;width:300px;height:500px;background-color:white;z-index:999;display:none;overflow-y:scroll;text-align:center";
     document.querySelector(".game-window-positioner").appendChild(gonitwa);
 
     const changeGonitwaState = function() {
