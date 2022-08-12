d68203204d9e746427960025c85d97d02a134bab
gswist0 <72512234+gswist0@users.noreply.github.com>
2022-08-12 12:40:29 +0200

    fixed endless loops

--- Auto heal z potek.user.js
+++ Auto heal z potek.user.js
@@ -1,20 +1,38 @@
 // ==UserScript==
 // @name         AutoHeal z potek i fullheal
-// @version      1.1
+// @version      1.2
 // @match        *.margonem.pl/
 // @grant        none
 // @author       Bancewald
 // ==/UserScript==
 
 //1.1 - dodane potki ktore lecza % hp
+//1.2 - naprawiono zapętlenia
 
 (Engine => {
+
+    const updateHeroHp = (item) => {
+        if(item._cachedStats.hasOwnProperty("leczy")){
+            Engine.hero.hp += item._cachedStats.leczy
+        }
+        else if(item._cachedStats.hasOwnProperty("fullheal")){
+            if(item._cachedStats.fullheal >= Engine.hero.d.maxhp - Engine.hero.d.hp){
+                Engine.hero.d.hp = Engine.hero.d.maxhp
+            } else {
+                Engine.hero.d.hp += item._cachedStats.fullheal
+            }
+        }
+        else if(item._cachedStats.hasOwnProperty("perheal")){
+            Engine.hero.hp += (item._cachedStats.perheal/100)*Engine.hero.maxhp
+        }
+    }
+
     const useItem = item => {
         const {
             name,
             id
         } = item;
-
+        updateHeroHp(item)
         window._g(`moveitem&st=1&id=${id}`, () => {
             setTimeout(autoHeal, 100);
         });
@@ -64,10 +82,10 @@
                 .filter(item => !item._cachedStats.hasOwnProperty("lvl") || (item._cachedStats.hasOwnProperty("lvl") && item._cachedStats.lvl <= lvl))
                 .filter(item => !item._cachedStats.hasOwnProperty("timelimit") || (item._cachedStats.hasOwnProperty("timelimit") && !item._cachedStats.timelimit.includes(",")));
 
-            var item;
-            if (items_fh.length > 0) item = getMaxHealValFH(items_fh);
+            let item;
+            if (items.length > 0) getMaxHealVal(items);
             else if (items_percent.length > 0) item = getMaxHealValperheal(items_percent);
-            else item = getMaxHealVal(items);
+            else if (items_fh.length > 0) item = getMaxHealValFH(items_fh);
 
             if (item !== undefined) {
                 useItem(item);
