83ef1863169492b6f6319155b10dc6d3cb540d7f
gswist0 <72512234+gswist0@users.noreply.github.com>
2022-09-05 23:43:26 +0200

    waits a while before moving

--- Antyduszek.user.js
+++ Antyduszek.user.js
@@ -1,11 +1,49 @@
 // ==UserScript==
 // @name         Antyduszek
-// @version      1.1
+// @version      1.2
 // @author       Bancewald
 // @match        *.margonem.pl/
 // ==/UserScript==
 
 //1.1 nie probuje ruszac sie gdy aktywna jest captcha
+//1.2 rusza sie po krotkim czasie
+
+function move(){
+    let isAfk = true
+    if(isAfk){
+        Engine.hero.autoGoTo({
+            x: Engine.hero.d.x,
+            y: Engine.hero.d.y + 1
+        })
+    } else return
+    Engine.hero.onSelfEmoList.forEach(emo => {
+        if(emo.name == "stasis") isAfk = true
+    })
+    if(isAfk){
+        Engine.hero.autoGoTo({
+            x: Engine.hero.d.x,
+            y: Engine.hero.d.y - 1
+        })
+    } else return
+    Engine.hero.onSelfEmoList.forEach(emo => {
+        if(emo.name == "stasis") isAfk = true
+    })
+    if(isAfk){
+        Engine.hero.autoGoTo({
+            x: Engine.hero.d.x + 1,
+            y: Engine.hero.d.y
+        })
+    } else return
+    Engine.hero.onSelfEmoList.forEach(emo => {
+        if(emo.name == "stasis") isAfk = true
+    })
+    if(isAfk){
+        Engine.hero.autoGoTo({
+            x: Engine.hero.d.x - 1,
+            y: Engine.hero.d.y
+        })
+    } else return
+}
 
 function run(){
     setInterval(() => {
@@ -15,39 +53,8 @@ function run(){
         Engine.hero.onSelfEmoList.forEach(emo => {
             if(emo.name == "stasis") isAfk = true
         })
-        if(isAfk){
-            Engine.hero.autoGoTo({
-                x: Engine.hero.d.x,
-                y: Engine.hero.d.y + 1
-            })
-        } else return
-        Engine.hero.onSelfEmoList.forEach(emo => {
-            if(emo.name == "stasis") isAfk = true
-        })
-        if(isAfk){
-            Engine.hero.autoGoTo({
-                x: Engine.hero.d.x,
-                y: Engine.hero.d.y - 1
-            })
-        } else return
-        Engine.hero.onSelfEmoList.forEach(emo => {
-            if(emo.name == "stasis") isAfk = true
-        })
-        if(isAfk){
-            Engine.hero.autoGoTo({
-                x: Engine.hero.d.x + 1,
-                y: Engine.hero.d.y
-            })
-        } else return
-        Engine.hero.onSelfEmoList.forEach(emo => {
-            if(emo.name == "stasis") isAfk = true
-        })
-        if(isAfk){
-            Engine.hero.autoGoTo({
-                x: Engine.hero.d.x - 1,
-                y: Engine.hero.d.y
-            })
-        } else return
+        if(isAfk)
+            setTimeout(move, 1000 + Math.random()*1000)
     },5000)
 }
 
